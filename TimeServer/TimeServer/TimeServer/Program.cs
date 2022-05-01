b189d74048a8adcfa410aca88443bda236f5ae17
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-05-01 14:55:07 +0200

    Initial setup

---
+++ TimeServer/TimeServer/TimeServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TimeServer;
+
+public static class Program {
+    static void Main(string[] arguments) 
+    {
+        
+    }
+}
