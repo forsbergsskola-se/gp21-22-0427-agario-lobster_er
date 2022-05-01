5548b6d3ca9e6f9793f64595c1e3e876571781f2
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-05-01 15:25:45 +0200

    TCP server

--- TimeServer/TimeServer/TimeServer/Program.cs
+++ TimeServer/TimeServer/TimeServer/Program.cs
@@ -1,12 +1,49 @@
-﻿using System.Net;
+﻿using System.Globalization;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.IO;
 
 namespace TimeServer;
 
 public static class Program {
-    static void Main(string[] arguments) 
+    static void Main(string[] arguments)
     {
-        
+        TcpListener? server = null;
+        try
+        {
+            server = new TcpListener(11);
+
+            server.Start();
+
+            for (;;)
+            {
+                Console.WriteLine("Connecting...");
+
+                TcpClient client = server.AcceptTcpClient();
+
+                Console.WriteLine("Connected!");
+
+                var response = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+
+                StreamWriter streamWriter = new StreamWriter(client.GetStream());
+                streamWriter.WriteLine(response);
+
+                Console.WriteLine("response: " + response);
+
+                streamWriter.Close();
+                client.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e + " " + e.StackTrace);
+            throw;
+        }
+        finally
+        {
+            server?.Stop();
+        }
     }
 }
