f1ad718d0733e3f87fe54b4d9dca64087f53f890
Armin Afazeli <90157472+Armin-AF@users.noreply.github.com>
2022-05-11 20:08:08 +0200

    OpenWorldClient

--- OpenWorldMMO/OpenWord-MMO/OpenWord-MMO/Program.cs
+++ OpenWorldMMO/OpenWord-MMO/OpenWord-MMO/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace OpenWord_MMO;
+
+public static class OpenWordClient
+{
+    static void Main()
+    {
+        IPEndPoint serverIpEndPoint = new IPEndPoint(IPAddress.Loopback, 11000);
+        IPEndPoint clientIpEndPoint = new IPEndPoint(IPAddress.Loopback, 11001);
+
+        while (true)
+        {
+            UdpClient client = new UdpClient(clientIpEndPoint);
+
+            Console.WriteLine("Send Your Message...");
+            
+            var stringInput = Console.ReadLine();
+
+            if (stringInput != null)
+            {
+                var message = Encoding.ASCII.GetBytes(stringInput);
+                client.Send(message, message.Length, serverIpEndPoint);
+
+                client.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
